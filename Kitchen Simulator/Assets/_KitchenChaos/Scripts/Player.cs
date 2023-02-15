e4940d0aa93a8995d39ca23bc3c69739a6184633
rahul-begames <rahulchandra99@gmail.com>
2023-02-15 20:24:44 +0530

    player controller ini

---
+++ Kitchen Simulator/Assets/_KitchenChaos/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]private float moveSpeed = 7f;
+    
+    private void Update()
+    {
+        //Input Vector should be vector2 instead of vector3 because technically speaking there are 2 input keys on the keyboard and not 2
+        Vector2 inputVector = new Vector2(0, 0);     
+       
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1f;
+        }
+        
+        inputVector = inputVector.normalized;
+        Vector3 moveVector = new Vector3(inputVector.x, 0, inputVector.y);
+        
+        
+        transform.position += moveVector * (moveSpeed * Time.deltaTime); 
+    }
+}
